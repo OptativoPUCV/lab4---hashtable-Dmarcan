a3ea78efddcfb23f119a4f664e44f8930d9746b0
Replit user <>
2023-04-11 00:35:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,7 +59,7 @@ struct HashMap {
 };
 */
 HashMap * createMap(long capacity) {
-    HashMap map = (HashMap *) malloc(sizeof(HashMap));
+    HashMap *map = (HashMap *) malloc(sizeof(HashMap));
     //if (map==NULL)return NULL;
     map->buckets = (pair **) calloc(capacity,sizeof(Pair *));
     //if (map->buckets==NULL)return NULL;
